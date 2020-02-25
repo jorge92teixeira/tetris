db1549295f9630e35d4cb3bdc80471776842c4de
Jorge Teixeira <jorgemrt@protonmail.com>
2020-02-25 10:36:11 +0000

    StyledCell

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => {
   return (
-    <div>
+    <StyledCell
+      type={'L'}
+      color={TETROMINOS['L'].color}
+    >
       Cell
-    </div>
+    </StyledCell>
   )
 }
 
